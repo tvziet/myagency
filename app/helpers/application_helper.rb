e20c577cd9f7cbb8ce65022c1f0a226bfc74040a
Viet Le <vietleuit@gmail.com>
2025-01-02 14:10:56 +0700

    refactor: :art: Remove logic codes in views, move logic to helpers
    Remove logic codes in views, move logic to helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,14 @@
 module ApplicationHelper
+  def page_slug(page)
+    I18n.locale == :en ? page.english_slug : page.slug
+  end
+
+  def myagency_info_data
+    myagency_info = MyAgencyInfo.first
+    {
+      address: I18n.locale == :en ? myagency_info.english_address : myagency_info.address,
+      phone_number: myagency_info.phone_number,
+      email: myagency_info.email
+    }
+  end
 end
